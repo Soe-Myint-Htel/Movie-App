77b9745bdab3207df77937fd48a58d379cc409fa
Soe Myint Htel <gretaoto44@gmail.com>
2022-02-22 17:23:47 -0800

    adding setting toggle

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 // menu-toggle
-document.querySelector("#menu-toggle").addEventListener("click", ()=>{
-    document.querySelector("#nav-menu").classList.toggle("nav-active");
+// document.querySelector("#menu-toggle").addEventListener("click", ()=>{
+//     document.querySelector("#nav-menu").classList.toggle("nav-active");
+// })
+
+// setting toggle
+document.querySelector("#setting-toggle").addEventListener("click", ()=>{
+    document.querySelector(".setting-box").classList.toggle("setting-box-active");
+});
+let images = Array.from(document.querySelectorAll(".setting-box img"));
+images.map((image)=>{
+    image.addEventListener("click", ()=>{
+        images.forEach((img)=>{
+            img.style.opacity = "1";
+        })
+        document.querySelector(".landing-image").src = image.src;
+        image.style.opacity = "0.4";
+    })
 })
